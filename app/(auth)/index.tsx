7dc576c0117e884e0d08d7aa896788962074026e
Seunghoon Han <seunghoon2118@gmail.com>
2024-12-24 19:46:54 +0700

    change login look

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -3,6 +3,9 @@ import "../../global.css";
 import { Link, useRouter } from 'expo-router';
 import { useState, useContext } from 'react';
 import { AuthContext, useAuth } from '@/providers/AuthProvider';
+import { Image } from 'react-native';
+
+
 
 export default function LoginScreen() {
   const router = useRouter();
@@ -13,8 +16,12 @@ export default function LoginScreen() {
 
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <View className='w-full p-4'>
-        <Text className='text-black font-bold text-3xl text-center py-2 mb-2'>Log in</Text>
+      <View className='w-full p-4 items-center'>
+        <Image
+          source={require('../../assets/images/s2icon.png')}
+          style={{ width: 100, height: 100, marginBottom: 20 }}
+        />
+        {/* <Text className='text-black font-bold text-3xl text-center py-2 mb-2'>Log in</Text> */}
       <TextInput
         placeholder='Email'
         className='bg-white p-4 rounded-lg border border-gray-300 w-full mb-3'
@@ -29,7 +36,7 @@ export default function LoginScreen() {
         onChangeText={setPassword}
       />
       <View className='py-2'>
-      <TouchableOpacity className='bg-black px-4 py-2 rounded-lg' onPress={()=>signIn(email,password)}>
+      <TouchableOpacity className='bg-red-500 px-4 py-2 rounded-lg' onPress={()=>signIn(email,password)}>
         <Text className=' text-white font-bold text-2xl text-center'>Login</Text>
       </TouchableOpacity>
       <Link href={"/(auth)/signup"} className='text-center py-6'>Dont have an account yet? Sign up here</Link>
